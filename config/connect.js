59a37e14e59026f1b5515674349fa8bfdad35be8
Oussama Missaoui <145665037+OussamaMi01@users.noreply.github.com>
2025-07-14 19:11:29 +0100

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connect = async () => {
     try {
-        const connection = await mongoose.connect("mongodb+srv://oussamamissaouiit:x3PoH6Q5uBdhm8Vf@cluster0.4ycox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const connection = await mongoose.connect("");
         console.log(`MongoDB connected: ${connection.connection.host}`);
 }catch (error) {
     console.error(`Error: ${error.message}`);
